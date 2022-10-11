81a354f2f15691a49419ad1ec1cae279ecc761a6
hairunisa29 <hairunisa2909@outlook.com>
2022-10-11 23:05:58 +0700

    create most picked section

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,13 +1,16 @@
-import React from "react";
+import React, { useRef } from "react";
 import Header from "parts/Header";
 import landingPage from "json/landingPage.json";
 import Hero from "parts/Hero";
+import MostPicked from "parts/MostPicked";
 
 const LandingPage = () => {
+  const refMostPicked = useRef();
   return (
     <>
       <Header />
-      <Hero data={landingPage.hero} />
+      <Hero refMostPicked={refMostPicked} data={landingPage.hero} />
+      <MostPicked refMostPicked={refMostPicked} data={landingPage.mostPicked} />
     </>
   );
 };
