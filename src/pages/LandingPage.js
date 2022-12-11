519ee20cefcd1a08dc993b2364c8773463e16c4e
hairunisa29 <hairunisa2909@outlook.com>
2022-12-11 22:53:39 +0700

    make component stars

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -4,6 +4,7 @@ import landingPage from "json/landingPage.json";
 import Hero from "parts/Hero";
 import MostPicked from "parts/MostPicked";
 import Categories from "parts/Categories";
+import Testimony from "parts/Testimony";
 
 const LandingPage = () => {
   const refMostPicked = useRef();
@@ -13,6 +14,7 @@ const LandingPage = () => {
       <Hero refMostPicked={refMostPicked} data={landingPage.hero} />
       <MostPicked refMostPicked={refMostPicked} data={landingPage.mostPicked} />
       <Categories data={landingPage.categories} />
+      <Testimony />
     </>
   );
 };
---
+++ src/parts/Testimony.js
@@ -0,0 +1,12 @@
+import Star from "elements/Star";
+import React from "react";
+
+const Testimony = () => {
+  return (
+    <div>
+      <Star value={4.5} width={35} height={35} spacing={4}></Star>
+    </div>
+  );
+};
+
+export default Testimony;
