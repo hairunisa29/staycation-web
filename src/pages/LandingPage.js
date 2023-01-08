58620b14e6f8ac84a8f7ea5c93bb7b56f554ba46
hairunisa29 <hairunisa2909@outlook.com>
2023-01-08 15:37:37 +0700

    add footer component

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -5,6 +5,7 @@ import Hero from "parts/Hero";
 import MostPicked from "parts/MostPicked";
 import Categories from "parts/Categories";
 import Testimony from "parts/Testimony";
+import Footer from "parts/Footer";
 
 const LandingPage = () => {
   const refMostPicked = useRef();
@@ -15,6 +16,7 @@ const LandingPage = () => {
       <MostPicked refMostPicked={refMostPicked} data={landingPage.mostPicked} />
       <Categories data={landingPage.categories} />
       <Testimony data={landingPage.testimonial} />
+      <Footer />
     </>
   );
 };
