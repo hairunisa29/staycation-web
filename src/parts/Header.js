f1e9561eb210564eaf41df1a873e92a38042005d
hairunisa29 <hairunisa2909@outlook.com>
2023-01-10 16:47:13 +0700

    add react reveal in landing pages

--- src/parts/Header.js
+++ src/parts/Header.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Fade from "react-reveal/Fade";
 import Button from "elements/Button";
 import BrandIcon from "parts/IconText";
 import "assets/scss/header.scss";
@@ -9,37 +10,39 @@ const Header = () => {
   };
 
   return (
-    <header className="spacing-sm">
-      <div className="container">
-        <nav className="navbar navbar-expand-lg navbar-light px-0">
-          <BrandIcon />
-          <div className="collapse navbar-collapse justify-content-end">
-            <ul className="navbar-nav">
-              <li className={`nav-item${getNavLinkClass("/")}`}>
-                <Button className="nav-link" type="link" href="/">
-                  Home
-                </Button>
-              </li>
-              <li className={`nav-item${getNavLinkClass("/browse-by")}`}>
-                <Button className="nav-link" type="link" href="/browse-by">
-                  Browse By
-                </Button>
-              </li>
-              <li className={`nav-item${getNavLinkClass("/stories")}`}>
-                <Button className="nav-link" type="link" href="/stories">
-                  Stories
-                </Button>
-              </li>
-              <li className={`nav-item${getNavLinkClass("/agents")}`}>
-                <Button className="nav-link" type="link" href="/agents">
-                  Agents
-                </Button>
-              </li>
-            </ul>
-          </div>
-        </nav>
-      </div>
-    </header>
+    <Fade>
+      <header className="spacing-sm">
+        <div className="container">
+          <nav className="navbar navbar-expand-lg navbar-light px-0">
+            <BrandIcon />
+            <div className="collapse navbar-collapse justify-content-end">
+              <ul className="navbar-nav">
+                <li className={`nav-item${getNavLinkClass("/")}`}>
+                  <Button className="nav-link" type="link" href="/">
+                    Home
+                  </Button>
+                </li>
+                <li className={`nav-item${getNavLinkClass("/browse-by")}`}>
+                  <Button className="nav-link" type="link" href="/browse-by">
+                    Browse By
+                  </Button>
+                </li>
+                <li className={`nav-item${getNavLinkClass("/stories")}`}>
+                  <Button className="nav-link" type="link" href="/stories">
+                    Stories
+                  </Button>
+                </li>
+                <li className={`nav-item${getNavLinkClass("/agents")}`}>
+                  <Button className="nav-link" type="link" href="/agents">
+                    Agents
+                  </Button>
+                </li>
+              </ul>
+            </div>
+          </nav>
+        </div>
+      </header>
+    </Fade>
   );
 };
 
--- src/parts/Hero.js
+++ src/parts/Hero.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Fade from "react-reveal/Fade";
 import ImageHero from "assets/images/img-hero.jpg";
 import ImageHeroFrame from "assets/images/img-hero-frame.jpg";
 import IconTraveler from "assets/images/icons/ic-traveler.svg";
@@ -17,83 +18,85 @@ const Hero = (props) => {
   };
 
   return (
-    <section className="container hero pt-4">
-      <div className="row align-items-center">
-        <div className="col pr-5">
-          <h1 className="font-weight-bold mb-3">
-            Forget Busy Work, <br />
-            Start Next Vacation
-          </h1>
-          <p
-            className="mb-4 font-weight-light w-75"
-            style={{ lineHeight: "170%" }}
-          >
-            We provide what you need to enjoy your holiday with family. Time to
-            make another memorable moments.
-          </p>
-          <Button
-            className="btn px-5"
-            hasShadow
-            isPrimary
-            onClick={onClickShowMostPicked}
-          >
-            Show Me Now
-          </Button>
-          <div className="row" style={{ marginTop: "80px" }}>
-            <div className="col-auto">
-              <img
-                src={IconTraveler}
-                alt={`${props.data.travelers} travelers`}
-                width="35"
-                height="35"
-              />
-              <h6 className="mt-3">
-                {formatNumber(props.data.travelers)} <span>Travelers</span>
-              </h6>
-            </div>
-            <div className="col-auto">
-              <img
-                src={IconTreasure}
-                alt={`${props.data.treasures} treasures`}
-                width="35"
-                height="35"
-              />
-              <h6 className="mt-3">
-                {formatNumber(props.data.treasures)} <span>Treasures</span>
-              </h6>
-            </div>
-            <div className="col-auto">
-              <img
-                src={IconCities}
-                alt={`${props.data.cities} cities`}
-                width="36"
-                height="36"
-              />
-              <h6 className="mt-3">
-                {formatNumber(props.data.cities)} <span>Cities</span>
-              </h6>
+    <Fade bottom>
+      <section className="container hero pt-4">
+        <div className="row align-items-center">
+          <div className="col pr-5">
+            <h1 className="font-weight-bold mb-3">
+              Forget Busy Work, <br />
+              Start Next Vacation
+            </h1>
+            <p
+              className="mb-4 font-weight-light w-75"
+              style={{ lineHeight: "170%" }}
+            >
+              We provide what you need to enjoy your holiday with family. Time
+              to make another memorable moments.
+            </p>
+            <Button
+              className="btn px-5"
+              hasShadow
+              isPrimary
+              onClick={onClickShowMostPicked}
+            >
+              Show Me Now
+            </Button>
+            <div className="row" style={{ marginTop: "80px" }}>
+              <div className="col-auto">
+                <img
+                  src={IconTraveler}
+                  alt={`${props.data.travelers} travelers`}
+                  width="35"
+                  height="35"
+                />
+                <h6 className="mt-3">
+                  {formatNumber(props.data.travelers)} <span>Travelers</span>
+                </h6>
+              </div>
+              <div className="col-auto">
+                <img
+                  src={IconTreasure}
+                  alt={`${props.data.treasures} treasures`}
+                  width="35"
+                  height="35"
+                />
+                <h6 className="mt-3">
+                  {formatNumber(props.data.treasures)} <span>Treasures</span>
+                </h6>
+              </div>
+              <div className="col-auto">
+                <img
+                  src={IconCities}
+                  alt={`${props.data.cities} cities`}
+                  width="36"
+                  height="36"
+                />
+                <h6 className="mt-3">
+                  {formatNumber(props.data.cities)} <span>Cities</span>
+                </h6>
+              </div>
             </div>
           </div>
-        </div>
 
-        <div className="col pl-5">
-          <div className="img-hero">
-            <img
-              src={ImageHero}
-              alt="Room with Couchces"
-              className="img-fluid position-absolute"
-              style={{ margin: "-30px 0 0 -30px", zIndex: 1 }}
-            />
-            <img
-              src={ImageHeroFrame}
-              alt="Room with Couchces Frame"
-              className="img-fluid position-absolute"
-              style={{ margin: "0 -15px -15px 0" }}
-            />
+          <div className="col pl-5">
+            <div className="img-hero">
+              <img
+                src={ImageHero}
+                alt="Room with Couchces"
+                className="img-fluid position-absolute"
+                style={{ margin: "-30px 0 0 -30px", zIndex: 1 }}
+              />
+              <img
+                src={ImageHeroFrame}
+                alt="Room with Couchces Frame"
+                className="img-fluid position-absolute"
+                style={{ margin: "0 -15px -15px 0" }}
+              />
+            </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </Fade>
   );
 };
 
