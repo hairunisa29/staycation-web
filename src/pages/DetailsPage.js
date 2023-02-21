886ede0b963d872e4b3c7a06910767fd715a8bf8
hairunisa29 <hairunisa2909@outlook.com>
2023-02-21 22:05:11 +0700

    create parts detail description

--- src/pages/DetailsPage.js
+++ src/pages/DetailsPage.js
@@ -3,6 +3,7 @@ import Header from "parts/Header";
 import PageDetailTitle from "parts/PageDetailTitle";
 import itemDetails from "json/itemDetails.json";
 import FeaturedImage from "parts/FeaturedImage";
+import PageDetailDescription from "parts/PageDetailDescription";
 
 const DetailsPage = () => {
   const breadcrumb = [
@@ -20,6 +21,16 @@ const DetailsPage = () => {
       <Header />
       <PageDetailTitle breadcrumb={breadcrumb} data={itemDetails} />
       <FeaturedImage data={itemDetails.imageUrls} />
+      <section className="container">
+        <div className="row">
+          <div className="col-7 pr-5">
+            <PageDetailDescription data={itemDetails} />
+          </div>
+          <div className="col-5">
+            Booking Form
+          </div>
+        </div>
+      </section>
     </>
   );
 };
