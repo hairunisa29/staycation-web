feb9fcf19d7a046a516255cbcafad7da5015116b
hairunisa29 <hairunisa2909@outlook.com>
2023-03-03 23:47:48 +0700

    fix inputNumber

--- src/elements/Form/InputNumber/index.js
+++ src/elements/Form/InputNumber/index.js
@@ -1,28 +1,20 @@
-import React, { useState } from "react";
+import React from "react";
 import propTypes from "prop-types";
 import "./index.scss";
 
 const Number = (props) => {
-  const { value, placeholder, name, min, max, prefix, suffix } = props;
-
-  const [inputValue, setInputValue] = useState(`${prefix}${value}${suffix}`);
+  const { value, placeholder, name, min, max, prefix, suffix, isSuffixPlural } = props;
 
   const onChange = (e) => {
     let value = String(e.target.value);
-    if (prefix) value = value.replace(prefix);
-    if (suffix) value = value.replace(suffix);
 
-    const patternNumeric = new RegExp("[0-9]*");
-    const isNumeric = patternNumeric.test(value);
-
-    if (isNumeric && +value <= max && +value >= min) {
+    if (+value <= max && +value >= min) {
       props.onChange({
         target: {
           name: name,
           value: +value,
         },
       });
-      setInputValue(`${prefix}${value}${suffix}`);
     }
   };
 
@@ -58,10 +50,10 @@ const Number = (props) => {
           min={min}
           max={max}
           name={name}
-          pattern="[0-9]*"
+          readOnly
           className="form-control"
           placeholder={placeholder ? placeholder : "0"}
-          value={String(inputValue)}
+          value={`${prefix}${value}${suffix}${isSuffixPlural && value > 1 ? "s" : ""}`}
           onChange={onChange}
         />
         <div className="input-group-append">
