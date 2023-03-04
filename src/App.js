6bb29a1190c46383d706a30bb1ee273f60a0bbee
hairunisa29 <hairunisa2909@outlook.com>
2023-03-04 17:16:20 +0700

    create page checkout

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "pages/LandingPage";
 import DetailsPage from "pages/DetailsPage";
+import Checkout from "pages/Checkout";
 import "assets/scss/style.scss";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/properties/:id" element={<DetailsPage />} />
+        <Route path="/checkout" element={<Checkout />} />
       </Routes>
     </Router>
   );
--- src/parts/Header.js
+++ src/parts/Header.js
@@ -4,10 +4,30 @@ import Button from "elements/Button";
 import BrandIcon from "parts/IconText";
 import "assets/scss/header.scss";
 
-const Header = () => {
+const Header = (props) => {
   const getNavLinkClass = (path) => {
     return window.location.pathname === path ? " active" : "";
   };
+
+  if (props.isCentered) {
+    return (
+      <Fade>
+        <header className="spacing-sm">
+          <div className="container">
+            <nav className="navbar navbar-expand-lg navbar-light">
+              <Button
+                className="brand-text-icon mx-auto"
+                href=""
+                type="link"
+              >
+                Stay<span className="text-gray-900">cation.</span>
+              </Button>
+            </nav>
+          </div>
+        </header>
+      </Fade>
+    );
+  }
 
   return (
     <Fade>
