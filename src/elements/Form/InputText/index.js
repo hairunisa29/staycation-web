fee52f6c48612f6b68e9c6ffa1c762264c51e23a
hairunisa29 <hairunisa2909@outlook.com>
2023-03-05 21:26:07 +0700

    create component input text

---
+++ src/elements/Form/InputText/index.js
@@ -0,0 +1,91 @@
+import React, { useState } from "react";
+import propTypes from "prop-types";
+import "./index.scss";
+
+export default function Text(props) {
+  const {
+    value,
+    type,
+    placeholder,
+    name,
+    prepend,
+    append,
+    outerClassName,
+    inputClassName,
+    errorResponse,
+  } = props;
+
+  const [isError, setIsError] = useState(null);
+  let pattern = "";
+  if (type === "email") pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (type === "tel") pattern = "[0-9]*";
+
+  const onChange = (e) => {
+    const target = {
+      name: name,
+      value: e.target.value,
+    };
+
+    if (type === "email") {
+      if (!pattern.test(e.target.value)) {
+        setIsError(errorResponse);
+      } else {
+        setIsError("");
+      }
+    }
+
+    if (type === "tel") {
+      if (e.target.validity.valid) {
+        props.onChange(target);
+      } else {
+        props.onChange(target);
+      }
+    }
+  };
+
+  return (
+    <div className={["input-text mb-3", outerClassName].join(" ")}>
+      <div className="input-group">
+        {prepend && (
+          <div className="input-group-prepend bg-gray-900">
+            <span className="input-group-text">{prepend}</span>
+          </div>
+        )}
+        <input
+          name={name}
+          type={type}
+          pattern={pattern}
+          className={["form-control", inputClassName].join(" ")}
+          value={value}
+          placeholder={placeholder}
+          onChange={onChange}
+        />
+        {append && (
+          <div className="input-group-append bg-gray-900">
+            <span className="input-group-text">{append}</span>
+          </div>
+        )}
+      </div>
+      {isError && <span className="error-helper">{isError}</span>}
+    </div>
+  );
+}
+
+Text.defaultProps = {
+  type: "text",
+  pattern: "",
+  placeholder: "Please type here...",
+  errorResponse: "Please match the requested format.",
+};
+
+Text.propTypes = {
+  name: propTypes.string.isRequired,
+  value: propTypes.oneOfType([propTypes.number, propTypes.string]).isRequired,
+  onChange: propTypes.func.isRequired,
+  prepend: propTypes.oneOfType([propTypes.number, propTypes.string]),
+  append: propTypes.oneOfType([propTypes.number, propTypes.string]),
+  type: propTypes.string,
+  placeholder: propTypes.string,
+  outerClassName: propTypes.string,
+  inputClassName: propTypes.string,
+};
