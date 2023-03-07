a56c1215028feb88cf3efeb48669e7fed5437888
hairunisa29 <hairunisa2909@outlook.com>
2023-03-07 23:19:26 +0700

    finish checkout page

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -1,12 +1,169 @@
-import React from 'react'
-import Header from 'parts/Header'
+import React, { useEffect, useState } from "react";
+import { Fade } from "react-reveal";
+import Header from "parts/Header";
+import Button from "elements/Button";
+import Stepper, {
+  Numbering,
+  Meta,
+  MainContent,
+  Controller,
+} from "elements/Stepper";
+import BookingInformation from "parts/Checkout/BookingInformation";
+import Payment from "parts/Checkout/Payment";
+import Completed from "parts/Checkout/Completed";
+import ItemDetails from "json/itemDetails.json";
 
 const Checkout = () => {
+  const initialState = {
+    firstName: "",
+    lastName: "",
+    email: "",
+    phone: "",
+    proofPayment: "",
+    bankName: "",
+    bankHolder: "",
+  };
+
+  const [data, setData] = useState(initialState);
+
+  const onChange = (e) => {
+    setData({ ...data, [e.target.name]: e.target.value });
+  };
+
+  useEffect(() => {
+    window.scroll(0, 0);
+  }, []);
+
+  const checkout = {
+    duration: 3,
+  };
+
+  const steps = {
+    bookingInformation: {
+      title: "Booking Information",
+      description: "Please fill up the blank fields below",
+      content: (
+        <BookingInformation
+          data={data}
+          checkout={checkout}
+          itemDetails={ItemDetails}
+          onChange={onChange}
+        />
+      ),
+    },
+    payment: {
+      title: "Payment",
+      description: "Kindly follow the instructions below",
+      content: (
+        <Payment
+          data={data}
+          itemDetails={ItemDetails}
+          checkout={checkout}
+          onChange={onChange}
+        />
+      ),
+    },
+    completed: {
+      title: "Yay! Completed",
+      description: null,
+      content: <Completed />,
+    },
+  };
+
   return (
     <>
-        <Header isCentered />
-    </>
-  )
-}
+      <Header isCentered />
+      <Stepper steps={steps}>
+        {(prevStep, nextStep, currentStep, steps) => (
+          <>
+            <Numbering
+              data={steps}
+              current={currentStep}
+              style={{ marginBottom: 50 }}
+            />
+            <Meta data={steps} current={currentStep} />
+            <MainContent data={steps} current={currentStep} />
 
-export default Checkout
+            {currentStep === "bookingInformation" && (
+              <Controller>
+                {data.firstName !== "" &&
+                  data.lastName !== "" &&
+                  data.email !== "" &&
+                  data.phone !== "" && (
+                    <Fade>
+                      <Button
+                        className="btn mb-3"
+                        type="button"
+                        isBlock
+                        isPrimary
+                        hasShadow
+                        onClick={nextStep}
+                      >
+                        Continue to Book
+                      </Button>
+                    </Fade>
+                  )}
+                <Button
+                  className="btn"
+                  type="link"
+                  isBlock
+                  isLight
+                  href={`/properties/${ItemDetails._id}`}
+                >
+                  Cancel
+                </Button>
+              </Controller>
+            )}
+
+            {currentStep === "payment" && (
+              <Controller>
+                {data.proofPayment !== "" &&
+                  data.bankName !== "" &&
+                  data.bankHolder !== "" && (
+                    <Fade>
+                      <Button
+                        className="btn mb-3"
+                        type="button"
+                        isBlock
+                        isPrimary
+                        hasShadow
+                        onClick={nextStep}
+                      >
+                        Continue to Book
+                      </Button>
+                    </Fade>
+                  )}
+                <Button
+                  className="btn"
+                  type="link"
+                  isBlock
+                  isLight
+                  onClick={prevStep}
+                >
+                  Cancel
+                </Button>
+              </Controller>
+            )}
+
+            {currentStep === "completed" && (
+              <Controller>
+                <Button
+                  className="btn"
+                  type="link"
+                  isBlock
+                  isPrimary
+                  hasShadow
+                  href=""
+                >
+                  Back to Home
+                </Button>
+              </Controller>
+            )}
+          </>
+        )}
+      </Stepper>
+    </>
+  );
+};
+
+export default Checkout;
