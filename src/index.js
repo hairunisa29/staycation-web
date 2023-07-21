b00cacec17ae39d5ed8522b19f40c3ceea1d80ac
hairunisa29 <hairunisa2909@outlook.com>
2023-07-21 13:42:02 +0700

    fix reactdom render

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "store";
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
